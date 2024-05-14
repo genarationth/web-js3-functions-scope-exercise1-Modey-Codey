e8042d67602c49df2690890efc5e5b3fd47a4f97
Modey Codey <phawngam@hotmail.com>
2024-05-14 13:55:26 +0700

    Ex 1.1 1.2

---
+++ script.js
@@ -0,0 +1,27 @@
+// EX1.1
+function addNum(a, b) {
+    return a + b;
+}
+console.log(addNum(3, 2));
+
+function subtractNum(a, b) {
+    return a - b;
+}
+console.log(subtractNum(3, 2));
+
+function multiplyNum(a, b) {
+    return a * b;
+}
+console.log(multiplyNum(3, 2));
+
+function divideNum(a, b) {
+    return a / b;
+}
+console.log(divideNum(3, 3));
+
+// Ex1.2
+function inform(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+hobby);
+}
+
+inform("Modtanoy", "Nonthaburi", "playing piano")
